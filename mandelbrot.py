af5dc8b512fd36121eef7929f240e14a9607eaf3
LieuweJS <baronlieuwe38@gmail.com>
2019-05-26 16:00:12 +0200

    rewrite navigation system

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,13 +4,12 @@ import colorsys
 from numpy import interp
 
 width = 400
+zoomSpeed = 2
 height = int(width/2)
 zoom = 1
 xMove = width
 yMove = height
-prevPressed = 0
-pressStarted = 0
-iterations = 60
+iterations = 120
 def rgb_conv(i):
     color = 255 * array(colorsys.hsv_to_rgb(i / 255.0, 1.0, 1.0))
     return tuple(color.astype(int))
@@ -22,36 +21,38 @@ def mandelbrot(x,y):
             return rgb_conv(i)
         c = c * c + c0
     return (0, 0, 0)
+
 pg.init()
 canvas = pg.display.set_mode((width, height))
-update = 1
+
+def reRender():
+    for y in range(height):
+        for x in range(width):
+            color = mandelbrot((x - (0.75 * width)) / (width / 4), (y - (width / 4)) / (width / 4))
+            canvas.set_at((x, y), color)
+reRender()
 while True:
     ev = pg.event.poll()
     if ev.type == pg.QUIT:
         break
-    if update == 1:
-        for x in range(width):
-            for y in range(height):
-                color = mandelbrot((x - (0.75 * width)) / (width / 4), (y - (width / 4)) / (width / 4))
-                canvas.set_at((x, y), color)
-        update = 0
     if pg.mouse.get_pressed()[0]:
-        if not pressStarted == 1:
-            pressStarted = 1
-            startPos = pg.mouse.get_pos()
-    elif not pg.mouse.get_pressed()[0] and pressStarted == 1:
-        prevPressed = 0
-        pressStarted = 0
         endPos = pg.mouse.get_pos()
-        zoom = 1#height/(abs(startPos[1] - endPos[1]))
-        xTemp = (endPos[0] - startPos[0])/2
-        xToMid = (endPos[0] - xTemp) - (width/2)
+        startPos = [width/2,height/2]
+        xToMid = (endPos[0] - startPos[0])
         xMove += interp(xToMid, [-width/2,width/2], [-2,2])
 
-        yTemp = (endPos[1] - startPos[1])/2
-        yToMid = (endPos[1] - yTemp) - (height/2)
+        yToMid = (endPos[1] - startPos[1])
         yMove += interp(yToMid, [-height/2,height/2], [-1,1])
-        update = 1
+        reRender()
+    elif ev.type == pg.MOUSEBUTTONDOWN:
+        if ev.button == 4:
+            #zoom in
+            zoom += zoomSpeed
+            reRender()
+        elif ev.button == 5:
+            zoom -= zoomSpeed
+            reRender()
+            #zoom out
     pg.draw.rect(canvas, (0,255,0), (width/2,1,1,height))
     pg.draw.rect(canvas, (0,255,0), (1,height/2,width,1))
     pg.display.flip()
